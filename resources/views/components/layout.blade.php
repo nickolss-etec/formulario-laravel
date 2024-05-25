d853513ccaedc2c5de2181cf437a89c030c05f57
nickolss-etec <nickolasmaraujo@gmail.com>
2024-05-25 18:29:11 -0300

    feat: Added content slot in the layout component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,4 @@
-@props(['title'])
+@props(['title', 'content'])
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,10 +8,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/button.css') }}">
 </head>
-<body>
+<body class="bg-slate-950 text-white">
     <x-header></x-header>
-    {{ $slot }}
+    {{ $content }}
     <x-footer></x-footer>
 </body>
 </html>
