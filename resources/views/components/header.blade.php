bee99c65d83fde71da33f71ded41a773a5d528c8
nickolss-etec <nickolasmaraujo@gmail.com>
2024-05-25 18:25:38 -0300

    feat: Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<header class="flex justify-around my-2">
+    <div class="flex items-center gap-10">
+        <img class="w-auto h-24" src="{{ asset('img/logo.png') }}" alt="Logo">
+        <h2><a href="{{ route('home') }}">Home</a></h2>
+        <nav>
+            <ul class="flex gap-4">
+                <a href="{{ route('about') }}">
+                    <li>Sobre Nós</li>
+                </a>
+                <a href="#">
+                    <li>Produtos</li>
+                </a>
+                <a href="#">
+                    <li>Catálogo</li>
+                </a>
+                <a href="{{ route('contact') }}">
+                    <li>Contato</li>
+                </a>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="flex items-center">
+        <button class="ui-btn">
+            <span>
+                Login
+            </span>
+        </button>
+    </div>
+</header>
