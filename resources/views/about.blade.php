e9908deda2da68113167f29f0dcd193650cfcf52
nickolss-etec <nickolasmaraujo@gmail.com>
2024-05-25 18:30:23 -0300

    feat: Added contact and about us view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Sobre nós">
+    <x-slot:content>
+        
+    </x-slot:content>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Contato">
+    <x-slot:content>
+        
+    </x-slot:content>
+</x-layout>
