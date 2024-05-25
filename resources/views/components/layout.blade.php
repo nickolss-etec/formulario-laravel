da9a3cc41167883116e17dccd20a0a8e924165aa
nickolss-etec <nickolasmaraujo@gmail.com>
2024-05-25 17:43:53 -0300

    fix: Added reference to Tailwind in layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <x-header></x-header>
